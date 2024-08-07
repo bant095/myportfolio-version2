94a3b64717cccb51d763886c6c62fae2163f6d81
AYODEJI ABOLAJI ALFRED <abolajiayodeji93@gmail.com>
2024-08-07 18:38:50 +0100

    I remove react-tsparticles pakages

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,13 +5,10 @@ import Hero from './sections/Hero';
 import MyServices from './sections/MyServices';
 import Skills from './sections/skill/Skills';
 import Testimonials from './sections/testimonials/Testimonials';
-import './landingpage.css';
-import ParticlesBackground from '../components/ParticlesBackground';
 
 const LandingPage = () => {
   return (
     <main className='relative overflow-hidden'>
-      <ParticlesBackground />
       <Hero />
       <MyServices />
       <Skills />
